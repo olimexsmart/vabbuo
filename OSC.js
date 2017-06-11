ec238bc930737743b5ebc9bb178cea3b5af0121b
Luca Olivieri <gliolivieri@live.it>
2017-06-11 13:42:17 +0200

    Added osc and main

---
+++ OSC.js
@@ -0,0 +1,8 @@
+// Returns a reference to the object. Is is structured to return again an object, to be more versatile
+function O(i) { return typeof i == 'object' ? i : document.getElementById(i); }
+
+// Return the style handle for an object or an id
+function S(i) { return O(i).style; }
+
+// Return an array of objects all being part of the same class
+function C(i) { return document.getElementByClassName(i); }
