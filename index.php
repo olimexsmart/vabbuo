caeb2563b27be14b2edf045b855c8d776e0cd3cb
Luca Olivieri <gliolivieri@live.it>
2017-08-14 11:25:26 +0200

    Reoganized files

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
     // Database connection
     require_once 'login.php';
     $sql = new mysqli($hostName, $userName, $passWord, $dataBase);
-if ($sql->connect_error) {
-    die($sql->connect_error);
-}
+    if ($sql->connect_error) {
+        die($sql->connect_error);
+    }
 
     // Getting informations
     $remote = ip2long($_SERVER['REMOTE_ADDR']);
@@ -14,69 +14,10 @@ if ($sql->connect_error) {
 
     $query = "INSERT INTO accessLog VALUES(NULL, '$remote', '$remoteString', '$userAgent', NULL)";
 
-if (!$sql->query($query)) {
-    echo "Could not insert into database: " . $sql->error;
-}
+    if (!$sql->query($query)) {
+        echo "Could not insert into database: " . $sql->error;
+    }
 
     $sql->close();
-?>
 
-
-<!DOCTYPE html>
-<html>
-    <style>
-	#mainButton {
-		position: absolute;
-		top:0;
-		bottom: 0;
-		left: 0;
-		right: 0;
-		margin:auto;
-		cursor: pointer;
-	}
-	* { margin: 0; padding: 0;}
-
-        body, html { height:100%; }
-    /*canvas { display: block; }*/
-	#c {
-	position:absolute;
-	width:100%;
-	height:100%;
-	}
-
-	@font-face {
-	font-family: "theconsolas";
-	src: url(Consolas.ttf) format("truetype");
-	}
-	
-    </style>
-    <head>
-    <meta name="viewport" content="width=device-width">
-        <title>Eh vabbuò</title>
-        <script src='OSC.js'></script>
-        <script src='utilities.js'></script>
-        <script src="fallingSentence.js"></script>
-        <script src="fallingSentenceManager.js"></script>
-        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
-    </head>
-        <body style="background-color: hsl(120, 100%, 35%);">
-
-                <canvas id="c"></canvas>
-        <canvas  id='mainButton' width='200' height='200'>
-            This text is visible only if you don't have HTML5, sorry dude we are in 2017
-        </canvas>
-
-    <script>
-        color = 120;
-        setInterval("rainbow(document.body, 100, 35)", 400);
-        drawButton(O('mainButton')); 
-                
-        canvas = $("#c");
-        updateCanvasDimensions(canvas);
-	    $(window).resize(updateCanvasDimensions(canvas));
-	                
-        fSM = new fallingSentenceManager(canvas, 8);
-    </script>
-    </body>
-</html>
-
+    echo file_get_contents("start.html");
--- main.php
+++ main.php
@@ -1,54 +1,23 @@
-<?php 
-	// Database connection
-	require_once 'login.php';
-	$sql = new mysqli($hostName, $userName, $passWord, $dataBase);
-	if ($sql->connect_error) die($sql->connect_error);
+<?php
+    // Database connection
+    require_once 'login.php';
+    $sql = new mysqli($hostName, $userName, $passWord, $dataBase);
+	if ($sql->connect_error) {
+		die($sql->connect_error);
+	}
 
-	// Getting informations
-	$remote = ip2long($_SERVER['REMOTE_ADDR']);
-	$remoteString = $_SERVER['REMOTE_ADDR'];
-	$headers = apache_request_headers();
-	$userAgent = $headers['User-Agent'];
+    // Getting informations
+    $remote = ip2long($_SERVER['REMOTE_ADDR']);
+    $remoteString = $_SERVER['REMOTE_ADDR'];
+    $headers = apache_request_headers();
+    $userAgent = $headers['User-Agent'];
 
-	$query = "INSERT INTO mainButton VALUES(NULL, '$remote', '$remoteString', '$userAgent', NULL)";
+    $query = "INSERT INTO mainButton VALUES(NULL, '$remote', '$remoteString', '$userAgent', NULL)";
 
-	if(!$sql->query($query))
+	if (!$sql->query($query)) {
 		echo "Could not insert into database: " . $sql->error;
+	}
 
-	$sql->close(); 
-?>
+    $sql->close();
 
-
-
-<!DOCTYPE html>
-<html>
-	<style>
-		@font-face {
-		font-family: "theconsolas";
-		src: url(Consolas.ttf) format("truetype");
-		}
-		/* Let's inherit to all DOM child the font */
-		body *{
-			font-family: 'theconsolas';
-		}
-    </style>
-	<head>
-    <meta name="viewport" content="width=device-width">
-        <title>Eh vabbuò</title>
-        <script src='OSC.js'></script>
-        <script src='utilities.js'></script>
-
-	</head>
-	<body style="background-color: hsl(120, 100%, 35%);">		
-		<form action="/submitSentence.php" method="post">
-			Nickname:<br> <input type="text" name="nickname"> <br>
-			Sentence:<br> <textarea name="sentence" cols="40" rows="5"></textarea> <br><br>
-					<input type="submit" value="Submit Sentence">
-		
-		<script>
-			color = 120;
-			setInterval("rainbow(document.body, 100, 35)", 400);
-		</script>
-	</body>
-</html>
-
+	echo file_get_contents("main.html");
