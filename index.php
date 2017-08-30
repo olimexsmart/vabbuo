21aa093ebc43a10f20de4b2a9e849def76f9f633
Luca Olivieri <gliolivieri@live.it>
2017-08-31 00:05:35 +0200

    Added geolocation

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+    // First load the page
+    echo file_get_contents("start.html");
+
     // Database connection
     require_once 'login.php';
     $sql = new mysqli($hostName, $userName, $passWord, $dataBase);
@@ -6,18 +9,19 @@
         die($sql->connect_error);
     }
 
-    // Getting informations
+    // Getting information
     $remote = ip2long($_SERVER['REMOTE_ADDR']);
     $remoteString = $_SERVER['REMOTE_ADDR'];
     $headers = apache_request_headers();
     $userAgent = $headers['User-Agent'];
 
-    $query = "INSERT INTO accessLog VALUES(NULL, '$remote', '$remoteString', '$userAgent', NULL)";
+    $geolocate = json_decode(file_get_contents('http://freegeoip.net/json/' . $remoteString), true);
+    $geolocation = $geolocate['country_name'] . ', ' . $geolocate['region_name'] . ', ' . $geolocate['city'];
+
+    $query = "INSERT INTO accessLog VALUES(NULL, '$remote', '$remoteString', '$geolocation', '$userAgent', NULL)";
 
     if (!$sql->query($query)) {
         echo "Could not insert into database: " . $sql->error;
     }
 
     $sql->close();
-
-    echo file_get_contents("start.html");
--- main.php
+++ main.php
@@ -1,23 +1,27 @@
 <?php
+
+    echo file_get_contents("main.html");
+
     // Database connection
     require_once 'login.php';
     $sql = new mysqli($hostName, $userName, $passWord, $dataBase);
-	if ($sql->connect_error) {
-		die($sql->connect_error);
-	}
+    if ($sql->connect_error) {
+        die($sql->connect_error);
+    }
 
-    // Getting informations
+    // Getting information
     $remote = ip2long($_SERVER['REMOTE_ADDR']);
     $remoteString = $_SERVER['REMOTE_ADDR'];
     $headers = apache_request_headers();
     $userAgent = $headers['User-Agent'];
 
-    $query = "INSERT INTO mainButton VALUES(NULL, '$remote', '$remoteString', '$userAgent', NULL)";
+    $geolocate = json_decode(file_get_contents('http://freegeoip.net/json/' . $remoteString), true);
+    $geolocation = $geolocate['country_name'] . ', ' . $geolocate['region_name'] . ', ' . $geolocate['city'];
 
-	if (!$sql->query($query)) {
-		echo "Could not insert into database: " . $sql->error;
-	}
+    $query = "INSERT INTO mainButton VALUES(NULL, '$remote', '$remoteString', '$geolocation', '$userAgent', NULL)";
+
+    if (!$sql->query($query)) {
+        echo "Could not insert into database: " . $sql->error;
+    }
 
     $sql->close();
-
-	echo file_get_contents("main.html");
