d65df4300113978dab1c9c2a25227089f51c06e9
Luca Olivieri <gliolivieri@live.it>
2017-10-01 15:15:41 +0200

    Revisited form

--- submitSentence.php
+++ submitSentence.php
@@ -13,14 +13,33 @@ $nickname = filter_var(isset($_POST['nickname']) ? $_POST['nickname'] : null, FI
 $sentence = filter_var(isset($_POST['sentence']) ? $_POST['sentence'] : null, FILTER_SANITIZE_STRING);
 $email = filter_var(isset($_POST['email']) ? $_POST['email'] : null, FILTER_SANITIZE_STRING);
 $sex = filter_var(isset($_POST['sex']) ? $_POST['sex'] : null, FILTER_SANITIZE_STRING);
-$contact = filter_var(isset($_POST['sentence']) ? 1 : 0, FILTER_SANITIZE_STRING);
+$contact = filter_var(isset($_POST['marpione']) ? 1 : 0, FILTER_SANITIZE_STRING);
+$other = filter_var(isset($_POST['other']) ? $_POST['other'] : null, FILTER_SANITIZE_STRING);
 
 // These come from the server, no need to sanitize
-$remote = ip2long($_SERVER['REMOTE_ADDR']);
-$remoteString = $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER['REMOTE_ADDR'];
 
+// Include and instantiate the class.
+require_once 'Mobile_Detect.php';
+$detect = new Mobile_Detect;
+$device = "";
+// First load the page
+if($detect->isMobile() && !$detect->isTablet()){
+    echo file_get_contents("mobileStart.html");
+    $device = "mobile";
+} else {
+    echo file_get_contents("start.html");
+    $device = "PC";
+}
+// Just check if tablet
+if($detect->isTablet()) {
+    $device = "tablet";
+}
 
-$query = "INSERT INTO newsentences VALUES(NULL, '$remote', '$remoteString', NULL, '$nickname', '$sentence', '$email', '$sex', '$contact', NULL)";
+$geolocate = json_decode(file_get_contents('http://freegeoip.net/json/' . $ip), true);
+$geolocation = preg_replace("/\'/", "\'",  $geolocate['country_name'] . ', ' . $geolocate['region_name'] . ', ' . $geolocate['city']);
+
+$query = "INSERT INTO newsentences VALUES(NULL, NULL, '$ip', '$device', '$geolocation', '$nickname', '$sentence', '$email', '$sex', '$contact', '$other')";
 
 if (!$sql->query($query)) {
     echo "Could not insert into database: " . $sql->error;
