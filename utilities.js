017d19ce70c4a807baf31eb92eb7c3610daa1072
Luca Olivieri <gliolivieri@live.it>
2017-10-31 14:27:05 +0100

    Button now gets refreshed, more complex animation necessary

--- utilities.js
+++ utilities.js
@@ -24,21 +24,21 @@ function updateCanvasDimensions(canvas) {
   canvas.attr({ height: $(window).height(), width: $(window).width() });
 }
 
-function drawButton(canvas, r, ri) {
-  // r = 100;
-  // ri = 70;     
+function drawButton(canvas, height, width) {
+  r = 100;
+  ri = 70;     
   context = canvas.getContext('2d');
   context.fillStyle = 'DarkGreen';
   canvas.addEventListener('click', function () { window.location.href = '/main.php'; }, false);
 
   // This part could be done also using the with(context) statement
   context.beginPath();
-  context.moveTo(r, r);
-  context.arc(r, r, r, 0, Math.PI * 2, false);
+  context.moveTo(width / 2, height / 2);
+  context.arc(width / 2, height / 2, r, 0, Math.PI * 2, false);
   context.fill();
   context.beginPath();
-  context.arc(r, r, ri, 0, Math.PI * 2, false);
-  context.fillStyle = 'green';
+  context.arc(width / 2, height / 2, ri, 0, Math.PI * 2, false);
+  context.fillStyle = "hsl(" + color + ",55%,30%)";
   context.fill();
   context.closePath();
 }
@@ -59,6 +59,8 @@ function animate(timestamp) {
   }
 
   fSM.drawAll(); // Sentence updating
+
+  drawButton(O('c'), fSM.canvasHeight, fSM.canvasWidth); // Draw button as last thing
 
   requestAnimationFrame(animate) // call requestAnimationFrame again to animate next frame  
 }
