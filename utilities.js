a3ef2579a338d03a56f83a7846c18440373a6b8c
Luca Olivieri <gliolivieri@live.it>
2017-11-06 08:06:59 +0100

    Fast code cleaning

--- utilities.js
+++ utilities.js
@@ -1,46 +1,6 @@
-// Completely useless stuff from initial page
-function timeUpdate(object) {
-  var date = new Date();
-  object.innerHTML = date.toTimeString().substr(0, 8) + "   " + formatDate(date);
-}
-function formatDate(date) {
-  var monthNames = [
-    "Gennaio", "Febbraio", "Marzo",
-    "Aprile", "Maggio", "Giugno", "Luglio",
-    "Agosto", "Settembre", "Ottobre",
-    "Novembre", "Dicembre"
-  ];
-
-  var day = date.getDate();
-  var monthIndex = date.getMonth();
-  var year = date.getFullYear();
-
-  return day + ' ' + monthNames[monthIndex] + ' ' + year;
-}
-
-
 // Useful stuff
 function updateCanvasDimensions(canvas) {
   canvas.attr({ height: $(window).height(), width: $(window).width() });
-}
-
-function drawButton(canvas, height, width) {
-  r = 100;
-  ri = 70;
-  context = canvas.getContext('2d');
-  context.fillStyle = 'DarkGreen';
-  canvas.addEventListener('click', function () { window.location.href = '/main.php'; }, false);
-
-  // This part could be done also using the with(context) statement
-  context.beginPath();
-  context.moveTo(width / 2, height / 2);
-  context.arc(width / 2, height / 2, r, 0, Math.PI * 2, false);
-  context.fill();
-  context.beginPath();
-  context.arc(width / 2, height / 2, ri, 0, Math.PI * 2, false);
-  context.fillStyle = "hsl(" + color + ",55%,30%)";
-  context.fill();
-  context.closePath();
 }
 
 function rainbow() {
